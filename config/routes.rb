445dc3f09a3f4c6b1088f3631783f5bb93a2b4d5
nobj <shgustjd6262@naver.com>
2018-05-15 02:41:30 +0000

    개인약관

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :fleemarkets
   root 'home#index'
   get 'home/info'
-
+  get 'home/priv'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
