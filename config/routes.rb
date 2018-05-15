8df21dda6db21d82dccb8e65494a84323d1f5faf
nobj <shgustjd6262@naver.com>
2018-05-15 13:23:44 +0000

    소공이 끝나고

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'home/info'
   get 'home/priv'
   get 'home/mypage'
+  get 'developers/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
